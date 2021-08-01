2975eb7e432afdede9a1d8154c9c55433038c6fa
Eduardo <dudarenz@gmail.com>
2021-08-01 20:10:00 -0300

    moved song component to folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { SongController } from './song.controller'
-import { SongService } from './song.service'
+import { SongModule } from './song/song.module'
 @Module({
-  imports: [],
-  controllers: [AppController, SongController],
-  providers: [AppService, SongService],
+  imports: [SongModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
